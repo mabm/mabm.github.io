00f35567b10ee12e2c3fef293f3db5e7f736b288
Joris Bertomeu <jbertomeu@baylibre.com>
2016-04-15 14:41:44 +0200

    'Back to top' button added

--- powerci/assets/js/powerci/Powers.page.js
+++ powerci/assets/js/powerci/Powers.page.js
@@ -94,6 +94,30 @@ var Powers = {
 		$('[id^=select-]').change(function() {
 			me.allowCompare($(this).attr('id'), $(this).attr('field'));
 		});
+
+		if ($('#back-to-top').length) {
+		    var scrollTrigger = 100,
+		        backToTop = function () {
+		            var scrollTop = $(window).scrollTop();
+		            if (scrollTop > scrollTrigger) {
+		                $('#back-to-top').addClass('show');
+		            } else {
+		                $('#back-to-top').removeClass('show');
+		            }
+		        };
+
+		    backToTop();
+		    $(window).on('scroll', function () {
+		        backToTop();
+		    });
+		    
+		    $('#back-to-top').on('click', function (e) {
+		        e.preventDefault();
+		        $('html,body').animate({
+		            scrollTop: 0
+		        }, 700);
+		    });
+		}
 	},
 	manageBeforeLoading: function() {
 		$('#noResult').hide();
